5ccceffce05046e7eba9831bc0bc9c8470502d8c
Sonic1231234 <azizbekismatov389@gmail.com>
2025-03-20 05:29:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/itex/')
-def itex():
-    return render_template('itex.html')
-
-@app.route('/dota/')
-def dota():
-    return render_template('Dota.html')
-
-@app.route('/blur/')
-def blur():
-    return render_template('Blur.html')
+def yellow():
+    return render_template('Yellow.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
